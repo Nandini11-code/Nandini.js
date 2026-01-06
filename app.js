db7eff33d1442f14fffe2f60a5b7d2fb9ad83c27
Nandini11-code <2025.nandinis@isu.ac.in>
2026-01-06 11:17:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// single line comment
+/**/ 
+// Strings are immutable
+let fname = "nandini";
+fname[0] = "N";
+console.log(fname); 
+console.log(fname.toUpperCase()); 
+
+// // concatenation
+let sayHello = "hello";
+console.log(sayHello, fname);
+console.log(sayHello + " " + fname);
+
+// // strings literals
+console.log(`${sayHello} ${fname}`)
+
+"use strict";
+let placement = null;
+console.log(placement);
